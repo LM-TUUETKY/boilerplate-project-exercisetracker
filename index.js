b944b34c16805ce8637106914ae20a18fafbc009
LM-TUUETKY <leky3516@gmail.com>
2025-04-29 10:58:40 +0700

    feat

--- index.js
+++ index.js
@@ -5,12 +5,71 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let users = [];
+let exercises = [];
+
+//tao nguoi dung moi
+app.post('/api/users', (req, res)=>{
+  const {username} = req.body;
+  const newUser = {username, _id:`id_${users.length+1}`};
+  users.push(newUser);
+  res.json(newUser);
+});
+
+app.post('/api/users/:_id/exercises', (req, res)=>{
+  const {_id} = req.params;
+  const {description} = req.body;
+  const {duration} = req.body;
+  const {date}=req.body;
+
+  const user = users.find(i => i._id === _id)
+  if(!user)
+    return res.status(404).json({error: "User not found"});
+  
+  const newExercises = {
+    username: user.username, 
+    description, 
+    duration:parseInt(duration), 
+    date: date ? new Date(date).toDateString() : new Date().toDateString(), 
+    _id: user._id,
+  };
+  exercises.push(newExercises);
+  res.json(newExercises);
+})
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  const { _id } = req.params;
+  const user = users.find(u => u._id === _id);
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  const userExercises = exercises.filter(ex => ex._id === _id);
+
+  const log = userExercises.map(ex => ({
+    description: ex.description,
+    duration: ex.duration,
+    date: ex.date
+  }));
+
+  res.json({
+    username: user.username,
+    count: log.length,
+    _id: user._id,
+    log
+  });
+});
+
+app.get('/api/log', (req, res)=>{
+  const {username, _id, description, duration, date} = req.body;
 
 
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
